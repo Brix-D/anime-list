18e716c0a5957c372b2b09d3c5873467f797c808
Brix-D <Shlyakhta4@gmail.com>
2022-07-30 21:44:32 +0400

    added vue axios and prettier rules

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import router from './router';
 
-import "./assets/css/index.css";
+import './assets/css/index.css';
 
 const app = createApp(App);
 
+app.use(VueAxios, axios);
+app.provide('axios', app.config.globalProperties.axios);
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+app.mount('#app');
