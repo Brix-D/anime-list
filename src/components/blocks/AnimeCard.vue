d68b8bfab915c1b3d3433a6d7a91a654895eb1d3
Brix-D <Shlyakhta4@gmail.com>
2022-09-04 20:18:25 +0400

    anime genres fix

--- src/components/blocks/AnimeCard.vue
+++ src/components/blocks/AnimeCard.vue
@@ -42,6 +42,7 @@
 
 <script setup lang="ts">
 import { computed } from 'vue';
+import type AnimeGenre from "@/components/types/AnimeGenre";
 
 interface Anime {
   mal_id: number;
@@ -50,7 +51,7 @@ interface Anime {
   status: string;
   synopsis: string;
   background?: string;
-  genres?: Array<object>;
+  genres?: Array<AnimeGenre>;
   images: AnimeImages;
   year?: number;
   url: string;
